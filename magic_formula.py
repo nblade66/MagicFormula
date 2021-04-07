0c7450689e74714a4deb0829a0e709c66a12ffc9
Nathan Hsu <NathanH.Hsu@gmail.com>
2021-04-06 22:40:29 -0400

    Bug fixes related to missing data and non-existent tickers. Implements batch fetching.

--- magic_formula.py
+++ magic_formula.py
@@ -136,6 +136,80 @@ def filter_tickers(cap):
     return ticker_temp
 
 
+# TODO Deal with problem when the ticker's data cannot be retrieved (ticker doesn't exist)
+def retrieve_data(batch_sz, tickers, metric):
+    batches = len(tickers) // batch_sz
+    for i in range(batches + 1):
+        start_loop = time.time()
+        ticker_sublist = tickers[i * batch_sz: min((i + 1) * batch_sz, len(tickers))]
+        if len(ticker_sublist) == 0:  # This is for when the batch_size evenly divides into the ticker_list size
+            break
+
+        yahoo_financials = YahooFinancials(ticker_sublist)
+
+        if metric == "balance":
+            print(f"Batch {i + 1}: Retrieving annual balance sheets from Yahoo Finance...")
+            balance_sheet.update(yahoo_financials.get_financial_stmts('annual', 'balance')['balanceSheetHistory'])
+
+            print(f"Saving batch {i + 1} to JSON file...")
+            json.dump(balance_sheet, open('annual_balance_sheet.json', 'w'))
+
+        elif metric == "income":
+            print(f"Batch {i + 1}: Retrieving annual income statement history from Yahoo Finance...")
+            income_statement.update(yahoo_financials.get_financial_stmts('annual', 'income')['incomeStatementHistory'])
+            print(f"Saving batch {i + 1} to JSON file...")
+            json.dump(income_statement, open('annual_income_statement.json', 'w'))
+
+        elif metric == "cap":
+            print(f"Batch {i + 1}: Retrieving market cap information from Yahoo Finance...")
+            market_cap.update(yahoo_financials.get_market_cap())
+            print(f"Saving batch {i + 1} to JSON file...")
+            json.dump(market_cap, open('market_cap_info.json', 'w'))
+
+        else:
+            print("Metric entered is not recognized.")
+
+        end_loop = time.time()
+
+        print(f"Time elapsed for batch {i + 1}: {end_loop - start_loop}")
+        print()
+
+
+# Checks balance_sheet, income_statement, and market_cap dictionaries for None values and removes those entries from
+# the dictionaries and the ticker_list, then updates their respective JSON files
+def clean_tickers():
+    none_tickers = set()
+    global balance_sheet, income_statement, market_cap, ticker_list
+    temp_balance = {}
+    temp_income = {}
+    temp_cap = {}
+    for k, v in balance_sheet.items():
+        if v is not None:
+            temp_balance[k] = v
+        else:
+            none_tickers.add(k)
+    for k, v in income_statement.items():
+        if v is not None:
+            temp_income[k] = v
+        else:
+            none_tickers.add(k)
+    for k, v in market_cap.items():
+        if v is not None:
+            temp_cap[k] = v
+        else:
+            none_tickers.add(k)
+
+    balance_sheet = temp_balance
+    json.dump(balance_sheet, open('annual_balance_sheet.json', 'w'))
+    income_statement = temp_income
+    json.dump(income_statement, open('annual_income_statement.json', 'w'))
+    market_cap = temp_cap
+    json.dump(market_cap, open('market_cap_info.json', 'w'))
+
+    ticker_list = [i for i in ticker_list if i not in none_tickers]
+    json.dump(ticker_list, open('ticker_list.json', 'w'))
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process refresh options')
     parser.add_argument('--refresh', '-r', action='store_true', dest='refresh', help='flag determines if we refresh the yahoo finance data')
@@ -146,9 +220,8 @@ if __name__ == '__main__':
 
     start = time.time()
 
-    # Save the most balance sheets that are fetched, so that I don't need to fetch every time. Have the option to
-    # refresh the balance sheets.
-
+    # Save the most recent balance sheets that are fetched, so that I don't need to fetch every time. Have the option to
+    # refresh the market caps.
     if args.refresh_tickers:
         print("Stripping text files to get list of stocks...")
         fhr = open('nasdaqlisted.txt', 'r')
@@ -173,51 +246,25 @@ if __name__ == '__main__':
     print("Retrieving ticker information from Yahoo Finance...")
     yahoo_financials = YahooFinancials(ticker_list)
 
+    batch_size = 10
+
     # TODO Modify to only update market caps of tickers that are in market_cap_info.json
     if not args.refresh and args.refresh_market_caps:
         print("Retrieving market cap information from Yahoo Finance...")
         market_cap = yahoo_financials.get_market_cap()
         json.dump(market_cap, open('market_cap_info.json', 'w'))
 
-    ticker_list = ['FB', 'UAL']
+    # (for debugging) ticker_list = ['FB', 'UAL', 'UAA', 'UAV']
 
     # If refreshing the ticker information (balance sheets, income statements, and market caps), scrape the data in
     # batches, and save in batches.
-    # TODO Accept a --continue argument that continues the web scraping using only tickers that aren't already in the
-    #   JSON data structure
     if args.refresh:
         balance_sheet = {}
         income_statement = {}
         market_cap = {}
-        batch_size = 1
-        batches = len(ticker_list) // batch_size
-        for i in range(batches + 1):
-            start_loop = time.time()
-            ticker_sublist = ticker_list[i * batch_size: min((i + 1) * batch_size, len(ticker_list))]
-            if len(ticker_sublist) == 0:    # This is for when the batch_size evenly divides into the ticker_list size
-                break
-
-            yahoo_financials = YahooFinancials(ticker_sublist)
-
-            print(f"Batch {i + 1}: Retrieving annual balance sheets from Yahoo Finance...")
-            balance_sheet.update(yahoo_financials.get_financial_stmts('annual', 'balance')['balanceSheetHistory'])
-
-            print(f"Batch {i + 1}: Retrieving annual income statement history from Yahoo Finance...")
-            income_statement.update(yahoo_financials.get_financial_stmts('annual', 'income')['incomeStatementHistory'])
-
-            print(f"Batch {i + 1}: Retrieving market cap information from Yahoo Finance...")
-            market_cap.update(yahoo_financials.get_market_cap())
-
-            # Use JSON to store balance sheets, income statements, and market cap
-            print(f"Saving batch {i + 1} to JSON file...")
-            json.dump(balance_sheet, open('annual_balance_sheet.json', 'w'))
-            json.dump(income_statement, open('annual_income_statement.json', 'w'))
-            json.dump(market_cap, open('market_cap_info.json', 'w'))
-
-            end_loop = time.time()
-
-            print(f"Time elapsed for batch {i+1}: {end_loop - start_loop}")
-            print()
+        retrieve_data(batch_size, ticker_list, "balance")
+        retrieve_data(batch_size, ticker_list, "income")
+        retrieve_data(batch_size, ticker_list, "cap")
     else:
         print("Loading annual balance sheets from json file...")
         with open('annual_balance_sheet.json') as json_file:
@@ -229,15 +276,25 @@ if __name__ == '__main__':
         with open('market_cap_info.json') as json_file:
             market_cap = json.load(json_file)
 
-
     if args.continue_refresh:
         # TODO Find what tickers are already in balance sheet, income statement, and market cap
         #   Create a new temp_ticker_list that has all tickers EXCEPT those that already have data
         #   Continue finding the data for the tickers in the temp_ticker_list (balance sheet, income, and market cap
         #   each get their own ticker list)
-        None
+        balance_keys = balance_sheet.keys()
+        print(balance_keys)
+        income_keys = income_statement.keys()
+        print(income_keys)
+        cap_keys = market_cap.keys()
+        print(cap_keys)
+        balance_sublist = [i for i in ticker_list if i not in balance_keys]
+        income_sublist = [i for i in ticker_list if i not in income_keys]
+        cap_sublist = [i for i in ticker_list if i not in cap_keys]
+        retrieve_data(batch_size, balance_sublist, "balance")
+        retrieve_data(batch_size, income_sublist, "income")
+        retrieve_data(batch_size, cap_sublist, "cap")
 
-
+    clean_tickers()
     update_db(ticker_list)
     ticker_list = filter_tickers(50000000)
 
